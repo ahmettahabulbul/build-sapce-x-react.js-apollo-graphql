32f4a372d554c93b54e3cd4597bbdd51f87043ca
ahmettahabulbul <l9118018@std.yildiz.edu.tr>
2022-08-24 01:39:20 +0300

    .

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,46 @@
+import React, { useRef } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import useNavigation from './../../hooks/useNavigation';
+import Error from './../Error';
+import Loader from './../Loader';
+import TopMenu from './../TopMenu';
+import SideMenu from './../SideMenu';
+import './style.scss';
+
+const GET_ROCKETS_NAMES = gql`
+    {
+        rockets(offset: 1) {
+            id
+            name
+        }
+    }
+`;
+const NavBar = () => {
+    const navRef = useRef(null);
+
+    const { isMobileView, isMenuOpen, setIsMenuOpen } = useNavigation(navRef);
+
+    const { data, loading, error } = useQuery(GET_ROCKETS_NAMES);
+    if (loading) return <Loader />;
+    if (error) return <Error error={error} />;
+    return (
+        <div className="container-fluid" ref={navRef}>
+            <div className="row">
+                <TopMenu
+                    isMobileView={isMobileView}
+                    isMenuOpen={isMenuOpen}
+                    toggleMenu={setIsMenuOpen}
+                    rockets={data.rockets}
+                />
+                <SideMenu
+                    isMobileView={isMobileView}
+                    isMenuOpen={isMenuOpen}
+                    toggleMenu={setIsMenuOpen}
+                    rockets={data.rockets}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
